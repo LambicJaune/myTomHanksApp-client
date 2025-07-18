25ae97afe51a1921a90a13a5d141dee8465f1be5
LambicJaune <thehopman@outlook.com>
2025-07-18 14:23:37 +0200

    Corrected wrong key
    replaced key={movie.id] to key={movie._id]

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -68,7 +68,7 @@ export const MainView = () => {
       ) : (
         <>
           {movies.map((movie) => (
-            <Col className="mb-5" key={movie.id} md={3}>
+            <Col className="mb-5" key={movie._id} md={3}>
               <MovieCard
                 movie={movie}
                 onMovieClick={(newSelectedMovie) =>
